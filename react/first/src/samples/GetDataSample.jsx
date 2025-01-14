047f2f33b3154babbf9b94f667f38e09aaf4dfdd
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2025-01-14 20:10:19 +0300

    14 ocak ders arası

--- react/first/src/samples/GetDataSample.jsx
+++ react/first/src/samples/GetDataSample.jsx
@@ -1,10 +1,20 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function GetDataSample() {
 
+
+  //let data = []  YANLIŞTIR ÇÜNKÜ GELEN DATA STATE ÜZERİNDE TUTULMALIDIR EKRANDA GÖSTERİLMEK İSTENİYORSA
+  const [data, setData] = useState([])
+
+
+  //LIFECYCLE
+  useEffect(() => {
+    getDataAxios()
+  },[])
+
   //FETCH İLE DATA ÇEKME
-  const getDataFetch = async() => {
+  const getDataFetch = async () => {
     try {
       let response = await fetch("https://rickandmortyapi.com/api/character")
       console.log(response)
@@ -16,20 +26,34 @@ function GetDataSample() {
   }
 
   //AXIOS İLE DATA ÇEKME
-  const getDataAxios = async() => {
+  const getDataAxios = async () => {
     try {
       let response = await axios.get("https://rickandmortyapi.com/api/character")
       console.log(response.data)
+      setData(response.data.results)
     } catch (error) {
       console.log('Get Data Error', error)
     }
   }
 
+  // () => {}  return yazmak zorundasın
+  // () => ()  return yazmak zorunda değilsin
 
   return (
     <div>
-      <button onClick={getDataFetch}>Get Data With Fetch</button>
-      <button onClick={getDataAxios}>Get Data With Axios</button>
+      {/* <button onClick={getDataFetch}>Get Data With Fetch</button>
+      <button onClick={getDataAxios}>Get Data With Axios</button> 
+      {
+        data.map((character, key) => (
+          <div key={key}>
+            <h1 style={{color: "tomato", fontWeight: "normal", textDecoration: "underline"}}>{character.name}</h1>
+            <h3 className="statusStyle">{character.status}</h3>
+          </div>
+        )
+        )
+      }
+      */}
+      
     </div>
   )
 }
